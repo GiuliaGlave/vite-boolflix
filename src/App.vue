53363f570f92c039ad889122c4b948fdb2e3a31c
GiuliaGlave <giulia.glave@gmail.com>
2023-03-07 19:13:03 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from "axios";
+import { store } from "./data/store";
+
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  components: {
+    AppHeader,
+    AppMain,
+  },
+
+  created() {
+    /* axios.get("movie-list").then((response) => {});
+    axios.get("tv-list").then((response) => {}); */
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+import AppCardMovie from "./AppCardMovie.vue";
+import AppCardTv from "./AppCardTv.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: {
+    AppCardMovie,
+    AppCardTv,
+  },
+};
+</script>
+
+<template>
+  <AppCardMovie />
+  <AppCardTv />
+</template>
+
+<style lang="scss" scoped></style>
