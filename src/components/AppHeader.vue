9c05118e63e397d5f7aca392804f589aca533c5f
GiuliaGlave <giulia.glave@gmail.com>
2023-03-07 20:00:26 +0100

    layout header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,6 +6,48 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <header class="bg-dark">
+    <div class="container">
+      <nav class="navbar w-100">
+        <div class="container-fluid">
+          <a class="navbar-brand fs-1 text-light">BOOLFLIX</a>
+          <form
+            class="d-flex"
+            role="search"
+          >
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              class="btn btn-outline-danger"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
+
+* {
+  font-family: "Bebas Neue", cursive;
+}
+header {
+  height: 5rem;
+  .container {
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
